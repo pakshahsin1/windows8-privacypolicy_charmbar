ad40125508a25ff821cb10bb50150094c4cae1fd
Syed Shahzad Khurshid <pakshahsin1@yahoo.com>
2013-04-04 21:01:52 +0500

    Privacy Policy in Charmbar example

---
+++ privacy_policyInCharmbar/privacy_policyInCharmbar/js/default.js
@@ -0,0 +1,32 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            args.setPromise(WinJS.UI.processAll());
+
+            WinJS.Application.onsettings = function (e) {
+                e.detail.applicationcommands = { "privacyPolicy": { title: "Privacy Policy", href: "/html/privacy_policy.html" } };
+                WinJS.UI.SettingsFlyout.populateSettings(e);
+            };
+        }
+    };
+
+
+
+    app.start();
+})();
